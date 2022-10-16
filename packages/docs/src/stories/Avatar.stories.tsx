c52eeec1a3fc0447ecb51b3a83d464b4bb48d6ee
Gabriel Borges <gbdesigns13@gmail.com>
2022-10-15 23:08:57 -0300

    Componente Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/GBDev13.png',
+    alt: 'GB Dev',
+  },
+} as Meta
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
