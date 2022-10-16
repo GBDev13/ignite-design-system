c927c90fc084192594081b25946bf47f2e8e1d71
Gabriel Borges <gbdesigns13@gmail.com>
2022-10-15 22:01:17 -0300

    Design System em React

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,18 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Enviar',
+  },
+} as Meta
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
