10dfca0b6f898fb4d652e2511777e02bd502ac94
Gabriel Borges <gbdesigns13@gmail.com>
2022-10-15 23:28:40 -0300

    Componente TextInput

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps } from '../../types/ComponentProps'
+import { Input, Prefix, TextInputContainer } from './styles'
+
+export type TextInputProps = ComponentProps<typeof Input> & {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...props} />
+    </TextInputContainer>
+  )
+}
